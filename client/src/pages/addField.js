c1b7fb984ea007d795d8121a2b68f2da4e39be43
Cody Chase <cchase1991@gmail.com>
2023-11-18 11:26:41 -0500

    changes to addField react component

--- client/src/pages/addField.js
+++ client/src/pages/addField.js
@@ -26,14 +26,22 @@ export const AddField = () => {
         setField({ ...field, [name]: value })
     }
 
-    return (
+    const onSubmit = (event) => {
+        
+    }
+    return ( 
         <div className="addField">
             <h2>Add Field</h2>
             <form>
+                <span className="formField">
                 <label htmlFor="fieldNumber">Field Number</label>
                 <input type="number" id="fieldNumber" name="fieldNumber" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="acres">Acres</label>
                 <input type="number" id="acres" name="acres" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="cropType">
                     Crop Type:
                     <select name="cropType" onChange={handleChange}>
@@ -54,28 +62,52 @@ export const AddField = () => {
                         <option value="fallow">Fallow</option>
                     </select>
                 </label>
+                </span>
+                <span className="formField">
                 <label htmlFor="plowed">Plowed</label>
                 <input type="checkbox" id="plowed" name="plowed" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="cultivated">Cultivated</label>
                 <input type="checkbox" id="cultivated" name="cultivated" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="limed">Limed</label>
                 <input type="checkbox" id="limed" name="limed" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="fertilized">Fertilized</label>
                 <input type="checkbox" id="fertilized" name="fertilized" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="weeded">Weeded</label>
                 <input type="checkbox" id="weeded" name="herbicide" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="mulched">Mulched</label>
                 <input type="checkbox" id="mulched" name="mulched" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="rolled">Rolled</label>
                 <input type="checkbox" id="rolled" name="rolled" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="perLoam">Percent of Loam</label>
                 <input type="number" id="perLoam" name="perLoam" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="perSandyLoam">Percent of Sandy Loam</label>
                 <input type="number" id="perSandyLoam" name="perSandyLoam" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="perLoamySand">Percent of Loamy Sand</label>
                 <input type="number" id="perLoamySand" name="perLoamySand" onChange={handleChange} />
+                </span>
+                <span className="formField">
                 <label htmlFor="perSiltyClay">Percent of Silty Clay</label>
                 <input type="number" id="perSiltyClay" name="perSiltyClay" onChange={handleChange} />
+                </span>
+                <button type="submit">Add Field</button>
             </form>
         </div>
     );
