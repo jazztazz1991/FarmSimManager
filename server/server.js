01ade4f43ecc174b122d7a3c6a8014d0259f8c62
Cody Chase <cchase1991@gmail.com>
2023-11-18 15:11:52 -0500

    server changes

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { userRouter } from './src/routes/users.js';
 import { fieldsRouter } from './src/routes/Fields.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
