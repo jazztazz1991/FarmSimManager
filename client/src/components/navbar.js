047d6a7d78906c456e0dc3b0f788309aea3e5627
Cody Chase <cchase1991@gmail.com>
2023-11-21 18:45:56 -0500

    missed a route

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -18,7 +18,7 @@ export const Navbar = () => {
     const fetchLastSavedDate = async () => {
         try {
             console.log("function ran")
-            const response = await axios.get("http://localhost:3001/site");
+            const response = await axios.get("/site");
             const siteInfo = response.data;
             let year = siteInfo.year;
             let monthCounter = siteInfo.monthCounter;
