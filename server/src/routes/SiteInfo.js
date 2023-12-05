3db4eff80ac30d29c49597becb45558595baa48c
Cody Chase <cchase1991@gmail.com>
2023-12-05 18:29:41 -0500

    removed a console log from siteinfo.js

--- server/src/routes/SiteInfo.js
+++ server/src/routes/SiteInfo.js
@@ -4,7 +4,6 @@ import { SiteInfoModel } from '../models/SiteInfo.js';
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    console.log("siteInfo is running!")
     const dateInfo = await SiteInfoModel.findById(process.env.siteInfoID);
     res.json(dateInfo);
 });
